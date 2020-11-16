27c3f612ade0c2c7b0ce470e9b16337e412be402
Emil InnerW <emil4uk273@gmail.com>
2020-11-16 13:01:13 +0400

    first

---
+++ src/FormElements/Main.java
@@ -0,0 +1,10 @@
+package FormElements;
+
+import FormElements.DrawForm;
+
+public class Main {
+    public static void main (String[] args) {
+        DrawForm drawForm = new DrawForm();
+        drawForm.workButtons();
+    }
+}
